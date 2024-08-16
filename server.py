ad4786ff85502bbe7651b9a55d48a8068574aea4
AryamanKejriwal <38677389+AryamanKejriwal@users.noreply.github.com>
2024-08-16 12:15:46 +0530

    Completed coding project. Name: Aryaman Kejriwal, UNI: ak4395

--- server.py
+++ server.py
@@ -12,25 +12,25 @@ scoreboard = [
 
     {
     "id": 2,
-    "name": "Tampa Bay Lightning", 
+    "name": "Tampa Bay Lightning",
     "score": 5
     },
 
     {
     "id": 3,
-    "name": "Toronto Maple Leafs", 
+    "name": "Toronto Maple Leafs",
     "score": 2
     },
 
     {
     "id": 4,
-    "name": "Florida Panthers", 
+    "name": "Florida Panthers",
     "score": 1
     },
 
     {
     "id": 5,
-    "name": "Buffalo Sabres", 
+    "name": "Buffalo Sabres",
     "score": 1
     },
 ]
@@ -38,25 +38,37 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    return render_template('scoreboard.html', scoreboard = scoreboard)
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
-    
+    json_data = request.get_json()
+    team_id = json_data["id"]
+
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
 
     return jsonify(scoreboard=scoreboard)
 
+@app.route('/reorder_rows', methods=['GET', 'POST'])
+def reorder_rows():
+
+    json_data = request.get_json()
+    index = json_data['index']
+    i = json_data['i']
+    for j in range(index, i, -1):
+        element = scoreboard[j]
+        scoreboard[j] = scoreboard[j-1]
+        scoreboard[j-1] = element
+
+    return jsonify({
+        'index': index,
+        'i': i
+    })
+
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -6,9 +6,9 @@ function display_scoreboard(scoreboard){
 }
 
 function addTeamView(id, name, score){
-  var team_template = $("<div class = row></div>");
+  var team_template = $("<div class = row id=" + id + "></div>");
   var name_template = $("<div class = col-md-5></div>");
-  var score_template = $("<div class = col-md-2></div>");
+  var score_template = $("<div class = col-md-2 id= score-" + id + "></div>");
   var button_template = $("<div class = col-md-2></div>");
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
@@ -23,16 +23,49 @@ function addTeamView(id, name, score){
   $("#teams").append(team_template);
 }
 
+function reorder_rows(id) {
+  let table = $("#teams");
+  let rows = table.children();
+  let index = 0;
+  while(parseInt(rows[index].id, 10) != id && index < rows.length) {
+    index += 1;
+  }
+
+  let i = index;
+
+  while(i != 0 && parseInt($("#score-" + rows[index].id).text(), 10) > parseInt($("#score-" + rows[i-1].id).text(), 10)) {
+    i -= 1;
+  }
+  if(index > i) {
+    $('#' + rows[index].id).insertBefore($('#' + rows[i].id));
+    return [index, i];
+  }
+
+  return [-1, -1];
+
+}
+
 function increase_score(id){
   var team_id = {"id": id}
   $.ajax({
     type: "POST",
-    url: "increase_score",                
+    url: "increase_score",
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      let score = parseInt($("#score-" + id).text(), 10) + 1;
+      $("#score-" + id).text(score);
+      [index, i] = reorder_rows(id);
+      if(index != -1) {
+        $.ajax({
+          type: "POST",
+          url: "reorder_rows",
+          dataType : "json",
+          contentType: "application/json; charset=utf-8",
+          data : JSON.stringify({index:index, i:i}),
+        });
+      }
     },
     error: function(request, status, error){
         console.log("Error");
